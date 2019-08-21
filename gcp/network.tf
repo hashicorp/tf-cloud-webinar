acd4637ce787b74553e5902ccc5a12b6259bb907
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-08-21 18:06:58 -0400

    Revert "Add backup network"
    This reverts commit 0e85efa35320f914969b9026fc7961e9ba802425.

--- gcp/network.tf
+++ gcp/network.tf
@@ -13,11 +13,11 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
 }
 
-resource "google_compute_subnetwork" "subnet_backup" {
-  name          = "${local.cluster}-subnet-backup"
-  ip_cidr_range = cidrsubnet(var.subnet_cidr, 4, 1)
-  network       = google_compute_network.vpc.name
-  depends_on    = [google_compute_network.vpc]
-  region        = var.region
-}
+// resource "google_compute_subnetwork" "subnet_backup" {
+//  name          = "${local.cluster}-subnet-backup"
+//  ip_cidr_range = cidrsubnet(var.subnet_cidr, 4, 1)
+//  network       = google_compute_network.vpc.name
+//  depends_on    = [google_compute_network.vpc]
+//  region        = var.region
+// }
 
